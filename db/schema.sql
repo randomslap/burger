7fa332afe2f36bb83070ea0dea168824a5d653f4
randomslap <kentokazaki@gmail.com>
2019-06-02 02:15:28 -0700

    fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
 drop database if exists burger_db;
 
+create database burger_db;
+
 use burger_db;
 
 create table burgers (
     id int AUTO_INCREMENT,
-    burger_name VARCHAR(255),
-    devoured boolean,
+    burger_name VARCHAR(255) not null,
+    devoured boolean not null,
     primary key (id)
 );
